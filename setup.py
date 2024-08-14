72981d5b837d839984e905c8d24c50d32c35b266
Wiep van der Toorn <wiep.vandertoorn@gmail.com>
2024-08-14 14:45:20 +0200

    feat: add version file

--- setup.py
+++ setup.py
@@ -5,7 +5,16 @@ from Cython.Build import cythonize
 from setuptools import Extension, find_packages, setup
 from setuptools.command.build_ext import build_ext
 
-VERSION = "0.4.2"
+import re
+
+VERSIONFILE = "warpdemux/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
 ext_modules = [
     Extension(
@@ -23,7 +32,7 @@ for e in ext_modules:
 setup(
     name="WarpDemuX",
     version=verstr,
-    packages=find_packages(),
+    packages=find_packages(exclude=["warpdemux/adapted*"]),
     author="Wiep van der Toorn",
     author_email="w.vandertoorn@fu-berlin.de",
     include_package_data=True,
--- warpdemux/__init__.py
+++ warpdemux/__init__.py
@@ -11,3 +11,7 @@ import os
 
 here = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(here, "adapted"))
+
+from warpdemux._version import __version__
+
+__all__ = ["__version__"]
